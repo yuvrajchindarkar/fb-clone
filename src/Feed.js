e278cf115728d31a839429dc3941a882afa7928c
viraj <yuvrajchindarkar90@gmail.com>
2023-07-02 00:31:44 +0530

    udated

--- src/Feed.js
+++ src/Feed.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Feed.css';
 import StoryReel from './StoryReel';
 import MessageSender from './MessageSender';
+import Post from './Post';
 
 function Feed() {
   return (
@@ -10,6 +11,17 @@ function Feed() {
         <StoryReel />
         {/* MessageSender */}
         <MessageSender />
+
+        <Post 
+        profilePic="https://tse3.mm.bing.net/th?id=OIP.TekkxTamvcGRTglOBTYAvwHaHa&pid=Api&P=0&h=180"
+        message="wow this works!"
+        timestamp="this is Timestamp"
+        username="kailas"
+        image="https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?cs=srgb&dl=mountains-nature-sky-459225.jpg&fm=jpg"
+         />
+
+        <Post/>
+        
     </div>
   )
 }
